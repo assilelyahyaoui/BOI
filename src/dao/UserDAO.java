d6929417b05106b48d78d2815b18d2a342d9dcfe
Melvil Donnart <melvildonnart3@gmail.com>
2018-03-09 12:46:22 +0100

    UserDAO

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -1,16 +1,24 @@
 package dao;
 
+import java.util.ArrayList;
+
 import models.User;
 
-public class UserDAO {
+public interface UserDAO {
 	
 	/**
 	 * Query the database to get the user by the pseudo given.
 	 * @param pseudo
 	 * @return User
 	 */
-	public User getUserByPseudo(String pseudo) {
-		//TODO : Implement the query
-		return null;
-	}
+	
+	public ArrayList<User> findAll();
+	public ArrayList<User> findById();
+	public ArrayList<User> findByName();
+	public boolean insertEmployee(User user);
+	public boolean updateEmployee(User user);
+	public boolean deleteEmployee(User user);
+
+	public User getUserByPseudo(String pseudo);
+	
 }
